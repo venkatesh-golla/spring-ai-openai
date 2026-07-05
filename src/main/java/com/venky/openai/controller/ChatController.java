1b7e2344ef67e4151de5432eee6c133c905d1dfe
venkatesh-golla <golla.venkatesh1997@gmail.com>
2026-07-04 22:35:51 -0700

    Restore application.yml to use openAi API key again Add new config class and move chatbuilder to the config class. Add default system parameter which is used as a system prompt

--- src/main/java/com/venky/openai/controller/ChatController.java
+++ src/main/java/com/venky/openai/controller/ChatController.java
@@ -11,12 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
   private final ChatClient chatClient;
 
-  public ChatController(ChatClient.Builder chatClientBuilder) {
-    this.chatClient = chatClientBuilder.build();
-  }
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
 
-  @GetMapping("/chat")
+
+    @GetMapping("/chat")
   public String chat(@RequestParam("message") String message) {
-    return chatClient.prompt(message).call().content();
+    return chatClient
+        .prompt()
+        .system(
+            """
+                       You are an internal IT helpdesk assistant. Your role is to assist employees with IT-related issues such as resetting passwords, unlocking accounts, and answering questions related to IT policies.
+                       If a user requests help with anything outside of these responsibilities, respond politely and inform them that you are only able to assist with IT support tasks within your defined scope.
+                        """)
+        .user(message)
+        .call()
+        .content();
   }
 }
