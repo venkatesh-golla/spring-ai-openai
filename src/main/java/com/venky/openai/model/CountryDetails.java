357686bb60149a2364bdd7ca0468d1e2880df5b2
venkatesh-golla <golla.venkatesh1997@gmail.com>
2026-07-06 20:04:50 -0700

    Add more details to CountryDetails record

--- src/main/java/com/venky/openai/model/CountryDetails.java
+++ src/main/java/com/venky/openai/model/CountryDetails.java
@@ -2,4 +2,4 @@ package com.venky.openai.model;
 
 import java.util.List;
 
-public record CountryDetails(String country, List<String> cities) {}
+public record CountryDetails(String country, List<String> cities, int population, String area) {}
